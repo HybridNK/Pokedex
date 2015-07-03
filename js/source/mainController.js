2bcfe8b58e7e1749ace1361668f63a67a820f757
Roger <marioroger11@gmail.com>
2015-07-03 14:59:28 -0500

    se añade lista de pokémon
    se agrega una lista con el contenedor de los sprites de cada pokémon, en
    futuro se conectara con la api para obtener las imagenes

--- js/source/mainController.js
+++ js/source/mainController.js
@@ -1,9 +1,37 @@
-angular.module('pokedex',[])
-.controller('main',['$scope',function($scope){
-    $scope.page = 'Pokedex';
+angular.module('pokedex', [])
+    .controller('main', ['$scope', function($scope) {
+        $scope.page = 'Pokedex';
+        $scope.pokemon = [{
+            'id': 1,
+            'name': 'pkmn'
+        }, {
+            'id': 2,
+            'name': 'pkmn'
+        }, {
+            'id': 3,
+            'name': 'pkmn'
+        }, {
+            'id': 4,
+            'name': 'pkmn'
+        }, {
+            'id': 5,
+            'name': 'pkmn'
+        }, {
+            'id': 6,
+            'name': 'pkmn'
+        }, {
+            'id': 7,
+            'name': 'pkmn'
+        }, {
+            'id': 8,
+            'name': 'pkmn'
+        }];
 
-    $scope.open = function(){
-        $scope.isOpen = true
-        console.warn('abriendo pokedex')
-    };
-}]);
+        $scope.getInfo = function(data){
+            alert(JSON.stringify(data))
+        };
+        $scope.open = function() {
+            $scope.isOpen = true
+            console.warn('abriendo pokedex')
+        };
+    }]);
