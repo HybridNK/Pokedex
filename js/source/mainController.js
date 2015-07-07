6baa1b97894ab5fcde41a78f7a9facb647f14f06
Roger <marioroger11@gmail.com>
2015-07-06 19:09:16 -0500

    se añade route en js
    Se añaden las rutas en angular

--- js/source/mainController.js
+++ js/source/mainController.js
@@ -1,39 +1,41 @@
-angular.module('pokedex', [])
-    .controller('main', ['$scope', function($scope) {
+angular.module('pokedex', ["ngRoute"])
+  .config(['$routeProvider',
+        function($routeProvider) {
+            var path = 'templates/';
+            console.log(path)
+           $routeProvider
+                .when('/obtenerInformacion/:tagId', {
+                    templateUrl: path + 'lista.html',
+                    controller: 'main'
+                })
+                .otherwise({
+                    templateUrl: path + 'home.html',
+                    controller: 'main'
+                });
+        }
+    ])
+    .controller('main', ['$scope','$location','$http',function($scope, $location, $http) {
         $scope.page = 'Pokedex';
-        $scope.pokemon = [{
-            'id': 1,
-            'name': 'pkmn'
-        }, {
-            'id': 2,
-            'name': 'pkmn'
-        }, {
-            'id': 3,
-            'name': 'pkmn'
-        }, {
-            'id': 4,
-            'name': 'pkmn'
-        }, {
-            'id': 5,
-            'name': 'pkmn'
-        }, {
-            'id': 6,
-            'name': 'pkmn'
-        }, {
-            'id': 7,
-            'name': 'pkmn'
-        }, {
-            'id': 8,
-            'name': 'pkmn'
-        }];
 
-        $scope.getInfo = function(){
-            console.log(arguments)
-            //alert(JSON.stringify(data))
+        $http.get('js/pkmn.json')
+            .success(function(data){
+                console.warn(data)
+                $scope.pokemon = data;
+            })
 
+        $scope.getInfo = function(pkmn){
+            $scope.showInfo = true
+            $location.path('/obtenerInformacion/'+pkmn.id);
         };
         $scope.open = function() {
             $scope.isOpen = true
             console.warn('abriendo pokedex')
         };
+        $scope.hideInfo = function(){
+           $location.path('')
+        };
+        $scope.close = function(){
+            $scope.isOpen = false
+        }
     }]);
+
